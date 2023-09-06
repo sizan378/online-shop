46ec3d9f94d78fb4dd3fcd46508482771f2fe2b3
sizan378 <sizan378>
2023-09-06 11:53:28 +0600

    ✨ feat(swagger): swagger add
    swagger add for api documentation

---
+++ docs/swagger.js
@@ -0,0 +1,75 @@
+/* Swagger configuration */
+const options = {
+    openapi: 'OpenAPI 3',   // Enable/Disable OpenAPI. By default is null
+    language: 'en-US',      // Change response language. By default is 'en-US'
+    disableLogs: false,     // Enable/Disable logs. By default is false
+    autoHeaders: false,     // Enable/Disable automatic headers capture. By default is true
+    autoQuery: false,       // Enable/Disable automatic query capture. By default is true
+    autoBody: false         // Enable/Disable automatic body capture. By default is true
+}
+
+// const config = require('../config/cloud');
+const swaggerAutogen = require('swagger-autogen')();
+// const msg = require('../utils/lang/messages');
+
+const doc = {
+  info: {
+    version: '2.0.0',      // by default: '1.0.0'
+    title: 'Online Apis',        // by default: 'REST API'
+    description: 'API for Managing queue calls',  // by default: ''
+    contact: {
+        'name': 'API Test',
+        'email': 'sizanmahmud08@gmail.com'
+    },
+  },
+  host: 'localhost:5002',      // by default: 'localhost:5002'
+  basePath: '/',  // by default: '/'
+  schemes: ['http'],   // by default: ['http']
+  consumes: ['application/json'],  // by default: ['application/json']
+  produces: ['application/json'],  // by default: ['application/json']
+  tags: [        // by default: empty Array
+    {
+      name: 'online-shop',         // Tag name
+      description: 'online-shop related apis',  // Tag description
+    },
+    {
+        name: 'online-shop',
+        description: 'Online shop Check'
+    }
+  ],
+  securityDefinitions: {},  // by default: empty object
+//   definitions: {
+//     helathResponse: {
+//       code: msg.response.CAG001.code,
+//       message: msg.response.CAG001.message,
+//     },
+//     'errorResponse.400': {
+//       code: msg.response.CAGE002.code,
+//       message: msg.response.CAGE002.message,
+//     },
+//     'errorResponse.403': {
+//       code: msg.response.CAGE001.code,
+//       message: msg.response.CAGE001.message,
+//     },
+//     'errorResponse.404': {
+//       "code": "404",
+//       "message": "Not found",
+//     },
+//     'errorResponse.500': {
+//       code: msg.response.CAGE003.code,
+//       message: msg.response.CAGE003.message,
+//     }
+//   },          // by default: empty object (Swagger 2.0)
+};
+
+const outputFile = './docs/swagger.json';
+const endpointsFiles = ['./index.js', './controllers/*.js'];
+
+/* NOTE: if you use the express Router, you must pass in the 
+   'endpointsFiles' only the root file where the route starts,
+   such as: index.js, app.js, routes.js, ... */
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+//     require('./index.js'); // Your project's root file
+//   });
